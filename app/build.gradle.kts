bd5b3cf7f3b4df7ae127ef336c1e70f184a65ef2
Joshua Kuestersteffen <jkuester@kuester7.com>
2020-07-04 07:31:46 -0500

    Set new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = 2
-        versionName = "0.0.2"
+        versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables{useSupportLibrary = true}
 
