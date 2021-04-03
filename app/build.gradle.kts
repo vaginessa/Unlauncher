9aabc0847211336da1c394b6afa76440e3e9b2d5
jkuester <jkuester@kuester7.com>
2021-04-03 11:23:39 -0500

    Prepare for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jkuester.unlauncher"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionName = "1.1.2"
-        versionCode = 9
+        versionName = "1.2.0"
+        versionCode = 10
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
 //        signingConfigs {
